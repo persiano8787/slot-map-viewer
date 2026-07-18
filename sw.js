0bec52c21e9e4c26d3f34845d28245a80a160769
persiano8787 <persiano8787@gmail.com>
2026-07-18 19:13:21 +0200

    feat: icona slot machine (any+maskable), cache v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'slotmap-v1';
+const CACHE = 'slotmap-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
